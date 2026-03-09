539fcc0aca53a425cfeb8213adabdb804a246275
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-03-09 11:57:57 +0700

    AlbyAnbiya

---
+++ nested_if.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a, float b)
+{
+    return (a + b) / 2;
+}
+
+string Status_lulus (float r)
+{
+    if (r >= 60)
+        return "Lulus";
+    else
+        return "Gagal";
+}
+
+int main ()
+{
+    float nilBI, nilMT, rerata;
+    string status;
+
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    rerata = Rata_rata(nilBI, nilMT);
+    status = Status_lulus(rerata);
+
+    cout << "Rata-rata = " << rerata << endl;
+    cout << "Status = " << status << endl;
+
+    return 0;
+}
